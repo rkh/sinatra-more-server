cbab7aa0b2d4d3b5849f1a675bc2b437de0a8dc7
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2010-02-26 17:51:36 +0100

    adjust specs

--- spec/sinatra/more_server_spec.rb
+++ spec/sinatra/more_server_spec.rb
@@ -11,12 +11,12 @@ describe Sinatra::MoreServer do
   end
 
   describe "async_server" do
-    it("should not offer ebb as an async server")     { app.async_server.should_not include("ebb")     }
     it("should not offer mongrel as an async server") { app.async_server.should_not include("mongrel") }
     it("should not offer webrick as an async server") { app.async_server.should_not include("webrick") }
     it("should not offer unicorn as an async server") { app.async_server.should_not include("unicorn") }
 
     it("should offer thin as an async server")     { app.async_server.should include("thin")     }
+    it("should offer ebb as an async server")      { app.async_server.should include("ebb")      }
     it("should offer rainbows as an async server") { app.async_server.should include("rainbows") }
     it("should offer zbatery as an async server")  { app.async_server.should include("zbatery")  }
 
